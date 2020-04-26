d5419f239e06ee2d26194c6761ddac4904c77522
qijiahao0426 <qijiahao0426@icloud.com>
2020-04-26 23:39:50 +0800

    企业信息

---
+++ src/main/java/cn/cyb/smart/bean/Enterprise.java
@@ -0,0 +1,100 @@
+package cn.cyb.smart.bean;
+
+public class Enterprise {
+    private int enterpriseId;
+    private String enterpriseName;
+    private String enterPriseLe;
+    private String contractNumber;
+    private String contact;
+    private int personNumber;
+    private int useState;
+    private String leaveReason;
+    private String contactPhone;
+
+    @Override
+    public String toString() {
+        return "Enterprise{" +
+                "enterpriseId=" + enterpriseId +
+                ", enterpriseName='" + enterpriseName + '\'' +
+                ", enterPriseLe='" + enterPriseLe + '\'' +
+                ", contractNumber='" + contractNumber + '\'' +
+                ", contact='" + contact + '\'' +
+                ", personNumber=" + personNumber +
+                ", useState=" + useState +
+                ", leaveReason='" + leaveReason + '\'' +
+                ", contactPhone='" + contactPhone + '\'' +
+                '}';
+    }
+
+    public int getEnterpriseId() {
+        return enterpriseId;
+    }
+
+    public void setEnterpriseId(int enterpriseId) {
+        this.enterpriseId = enterpriseId;
+    }
+
+    public String getEnterpriseName() {
+        return enterpriseName;
+    }
+
+    public void setEnterpriseName(String enterpriseName) {
+        this.enterpriseName = enterpriseName;
+    }
+
+    public String getEnterPriseLe() {
+        return enterPriseLe;
+    }
+
+    public void setEnterPriseLe(String enterPriseLe) {
+        this.enterPriseLe = enterPriseLe;
+    }
+
+    public String getContractNumber() {
+        return contractNumber;
+    }
+
+    public void setContractNumber(String contractNumber) {
+        this.contractNumber = contractNumber;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public int getPersonNumber() {
+        return personNumber;
+    }
+
+    public void setPersonNumber(int personNumber) {
+        this.personNumber = personNumber;
+    }
+
+    public int getUseState() {
+        return useState;
+    }
+
+    public void setUseState(int useState) {
+        this.useState = useState;
+    }
+
+    public String getLeaveReason() {
+        return leaveReason;
+    }
+
+    public void setLeaveReason(String leaveReason) {
+        this.leaveReason = leaveReason;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+}
